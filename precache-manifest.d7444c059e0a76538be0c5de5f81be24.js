f1a83383b78c95fd1e63766d2510b6bdd94a1435
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2020-10-13 06:29:23 +0000

    Deploy ConsumerDataStandardsAustralia/banking-products-comparator to github.com/ConsumerDataStandardsAustralia/banking-products-comparator.git:gh-pages

---
+++ precache-manifest.d7444c059e0a76538be0c5de5f81be24.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "737a59f3790f85c3a7b094edcd0b75b2",
+    "url": "/banking-products-comparator/index.html"
+  },
+  {
+    "revision": "a9771ae5fdfc77b3448d",
+    "url": "/banking-products-comparator/static/css/main.ed964756.chunk.css"
+  },
+  {
+    "revision": "b38cd46303aa2d85aed4",
+    "url": "/banking-products-comparator/static/js/2.30803f4b.chunk.js"
+  },
+  {
+    "revision": "7343f094378f09d024faeb054aed8cdc",
+    "url": "/banking-products-comparator/static/js/2.30803f4b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a9771ae5fdfc77b3448d",
+    "url": "/banking-products-comparator/static/js/main.20b2d938.chunk.js"
+  },
+  {
+    "revision": "787fbc7cebc981bd284a",
+    "url": "/banking-products-comparator/static/js/runtime-main.ad2c1b2f.js"
+  }
+]);
